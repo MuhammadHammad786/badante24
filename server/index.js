4ce5b50150a3218d2d9e9524b4ac057320ee7eef
MuhammadHammad786 <muhammadhammad842988@gmail.com>
2021-05-17 04:46:13 +0500

    make it better

--- server/index.js
+++ server/index.js
@@ -189,11 +189,11 @@ if(process.env.NODE_ENV=="production")
 	})
 }
 
-app.use(express.static('../client/build'))
-const path = require('path')
-app.get("*", (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-})
+// app.use(express.static('../client/build'))
+// const path = require('path')
+// app.get("*", (req, res) => {
+// 	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+// })
 
 
 app.listen(app.get('port'), function () {
